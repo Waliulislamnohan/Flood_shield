36eff4e31f62238cf04db8bfa71ffc36e899a5c3
MD Waliul Islam Nohan <waliulnohan@gmail.com>
2024-10-06 09:36:06 +0600

    updated

--- src/app/pages/prediction/page.js
+++ src/app/pages/prediction/page.js
@@ -3,12 +3,16 @@
 import DashboardLayout from "@/app/layout/DashboardLayout";
 
 // Dynamically import the component to avoid server-side rendering issues with Leaflet
-import MapCoordinates from "@/app/components/prediction";
+//import MapCoordinates from "@/app/components/prediction";
+import dynamic from 'next/dynamic';
+
+// Dynamically import the Map component and disable SSR
+const DynamicMap = dynamic(() => import('@/app/components/prediction'), { ssr: false });
 
 export default function MapPage() {
   return (
     <DashboardLayout>
-        <MapCoordinates />
+        <DynamicMap />
     </DashboardLayout>      
     
   );
